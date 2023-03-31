0c9739e59e31c84207b6a4ca13005fef40e4ae41
Pham Tien Anh <phamtienanhdev@gmail.com>
2023-03-31 22:58:09 +0700

    LeetCode

--- _53_Maximum_Subarray/Solution.java
+++ _53_Maximum_Subarray/Solution.java
@@ -1,16 +1,16 @@
 package _53_Maximum_Subarray;
 
 public class Solution {
-  public static int maxSubArray(int[] nums) {
-    
+  public static int BruteForce(int[] nums) {
+    int n = nums.length;
     int cur = nums[0];
     int max = cur;
-    for (int i = 0; i < nums.length; i++) {
+    for (int i = 0; i < n; i++) {
        cur = nums[i];
       if(cur > max){
         max = cur;
       }
-      for (int j = i + 1; j < nums.length; j++) {
+      for (int j = i + 1; j < n; j++) {
      cur = cur + nums[j];
      if(cur > max ){
      max = cur;
@@ -19,8 +19,27 @@ public class Solution {
         }
         return max;
   }
+  //-----------------------------------------
+  public static int Kadane(int[] nums) {
+    int max = Integer.MIN_VALUE;
+    int sum = 0;
+    int n = nums.length;
+    for (int i = 0; i < n; i++) {
+      sum += nums[i];
+      if(sum > max){
+        max = sum;
+      }
+      if(sum < 0){
+        sum = 0;
+      }
+    }
+    System.gc();
+    return max;
+  }
+
   public static void main(String[] args) {
     int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
-    System.out.println(maxSubArray(nums));
+    System.out.println(BruteForce(nums));
+    System.out.println(Kadane(nums));
   }
 }
---
+++ _57_Insert_Interval/Solution.java
@@ -0,0 +1,33 @@
+package _57_Insert_Interval;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+  public static int[][] insert(int[][] intervals, int[] newInterval) {
+    final int n = intervals.length;
+    int i = 0;
+    List<int[]> ans = new ArrayList<>();
+    while (i < n && intervals[i][1] < newInterval[0]) {
+      ans.add(intervals[i++]);
+    }
+    System.out.println(Arrays.toString(intervals[++i]));
+    while (i < n && intervals[i][0] <= newInterval[1]) {
+      newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+      newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+      ++i;
+    }
+    ans.add(newInterval);
+    while (i < n) {
+      ans.add(intervals[i++]);
+    }
+    return ans.stream().toArray(int[][] ::new);
+
+  }
+  public static void main(String[] args) {
+    int[][] insert = {{1,3},{6,9}};
+    int[] newInterval = {2,5};
+    System.out.println(Arrays.toString(insert(insert, newInterval)));
+  }
+}
